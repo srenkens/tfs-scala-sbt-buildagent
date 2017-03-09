fdfa409c99db46439be515fa471c8fc0c18b4277
Sebastiaan Renkens <sebastiaan.renkens@ordina.nl>
2017-03-09 16:23:53 +0100

    Make the agent more restart tolerant

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,26 @@
 #!/bin/bash 
+set -euo pipefail
+
+# X Config
+if [ -f /tmp/.X99-lock ]; then
+  rm -rf /tmp/.X99*
+fi
+export XVFB_WHD=${XVFB_WHD:-1280x720x16}
+export DISPLAY=:99
+echo Starting X
+Xvfb :99 -ac -screen 0 $XVFB_WHD -nolisten tcp &
+
+
+
+
 echo "$1"
-
 cd /myagent
-
 export USER_ID=user
 
 case "$1" in
   install)
-    service dbus start	
+	rm -rf /var/run/dbus/*
+    service dbus start
     gosu $USER_ID ./run-agent.sh;
     ;;
   remove)
--- run-agent.sh
+++ run-agent.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
+set -euo pipefail
 
 # NodeJS Config
 #export CHROME_BIN=/usr/bin/chromium-browser
 export CHROME_BIN=/usr/bin/google-chrome
 export NODE_ENV=CI
 
-# X Config
-export XVFB_WHD=${XVFB_WHD:-1280x720x16}
-export DISPLAY=:99
-Xvfb :99 -ac -screen 0 $XVFB_WHD -nolisten tcp &
+
 
 # start VNC for chrome debugging
 x11vnc &
