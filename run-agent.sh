abe0ccc19adaf02ac2cd8e794361dfe4c48195db
Sebastiaan Renkens <sebastiaan.renkens@ordina.nl>
2017-03-13 11:23:41 +0100

    Fixes: ./env.sh: line 31: !checkVar: unbound variable

--- run-agent.sh
+++ run-agent.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-set -euo pipefail
+set -euo pipefail # Strict mode on
 
 # NodeJS Config
-#export CHROME_BIN=/usr/bin/chromium-browser
 export CHROME_BIN=/usr/bin/google-chrome
 export NODE_ENV=CI
-
 
 
 # start VNC for chrome debugging
 x11vnc &
 
+
 # Configure and Run the TFS Agent
+set +u # Allows unset variables (the env.sh script of the buildagent failes at this part)
 . ./config.sh --unattended --acceptteeeula --replace --url ${TFSURL} --auth PAT --token ${TFSTOKEN} --pool ${TFSPOOL} --agent ${TFSAGENT}
+set -u # Strict mode on
 . ./run.sh;
